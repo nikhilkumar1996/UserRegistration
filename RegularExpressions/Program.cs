eccecd74a3355e6f4f80b31b6ce60c18f1adeee6
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-18 03:43:52 +0530

    [Nikhil Kumar]:FirstName Validation UC Added

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -11,6 +11,27 @@ namespace RegularExpressions
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            Console.WriteLine("1.First Name Validation" +
+                              "\n2.Exit");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Options");
+                int choose=Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        Console.WriteLine("User Registration Validating using Regular Expression!");
+                        FirstNameRegEx regexSample = new FirstNameRegEx();
+                        regexSample.Validating();
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
