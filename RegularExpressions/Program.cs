ece9b77997e185432f5aaee57bea77229f76cb11
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-28 02:42:59 +0530

    [Nikhil Kumar]:Validating Using Regular Expression

--- RegularExpressions/Program.cs
+++ RegularExpressions/Program.cs
@@ -10,61 +10,10 @@ namespace RegularExpressions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Program");
-            Console.WriteLine("1.First Name Validation" +
-                              "\n2.Last Name Validation" +
-                              "\n3.Email Id" +
-                              "\n4.Phone Number" +
-                              "\n5.PasswordCheck" +
-                              "\n6.Exit");
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine("Choose Options");
-                int choose=Convert.ToInt32(Console.ReadLine());
-
-                switch (choose)
-                {
-                    case 1:
-                        Console.WriteLine("User Registration Validating using Regular Expression!");
-                        FirstNameRegEx regexSample = new FirstNameRegEx();
-                        regexSample.Validating();
-                        break;
-
-                    case 2:
-                        Console.WriteLine("User Registration Validating using Regular Expression!");
-                        LastNameRegex regexSample1 = new LastNameRegex();
-                        regexSample1.Validating();
-                        break;
-                    case 3:
-                        Console.WriteLine("User Registration Validating using Regular Expression!");
-                        EmailID emailID = new EmailID();
-                        emailID.Validating();
-                        break;
-
-                    case 4:
-                        Console.WriteLine("User Registration Validating using Regular Expression!");
-                        PhoneNoRegex phno = new PhoneNoRegex();
-                        phno.Validating();
-                        break;
-
-                    case 5:
-                        Console.WriteLine("User Registration Validating using Regular Expression!");
-                        PasswordCheckRegex passwordCheckRegex = new PasswordCheckRegex();
-                        passwordCheckRegex.Validating();
-                        break;
-
-
-
-                    case 6:
-                        flag = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid Option");
-                        break;
-                }
-            }
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
         }
+      
     }
 }
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,143 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegularExpressions;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [TestClass]
+        public class UnitTest1
+        {
+            //validation for first Name
+            [TestMethod]
+            [DataRow("Nikhil", "Nikhil")]
+            [DataRow("Nik", "Nik")]
+            [DataRow("N", null)]
+            [DataRow("Nikhil05", null)]
+
+            public void ValidateFirstname(string a, string expected)
+            {
+                try
+                {
+
+                    var actual = RegexSample.ValidatingFirstName(a);
+                    Assert.AreEqual(expected, actual);
+                }
+                catch (CustomException actual)
+                {
+                    Assert.AreEqual(expected, actual.message);
+                }
+            }
+            //Validating for Last Name
+            [TestMethod]
+            [DataRow("Kumar", "Kumar")]
+            [DataRow("Ku", null)]
+            [DataRow("kumar", "kumar")]
+            [DataRow("Kumar05", null)]
+            public void ValidateUserLastname(string a, string expected)
+            {
+                try
+                {
+                    var actual = RegexSample.ValidatingLastName(a);
+                    Assert.AreEqual(expected, actual);
+                }
+                catch (CustomException actual)
+                {
+                    Assert.AreEqual(expected, actual.message);
+                }
+            }
+            //Validation for Email
+            [TestMethod]
+            [DataRow("abc@gmail.com", "abc@gmail.com")]
+            [DataRow("abc-100@yahoo.com", "abc-100@yahoo.com")]
+            [DataRow("abc.100@yahoo.com", "abc.100@yahoo.com")]
+            [DataRow("abc111@yahoo.com", "abc111@yahoo.com")]
+            [DataRow("abc111@abc.com", "abc111@abc.com")]
+            [DataRow("abc-100@abc.net", "abc-100@abc.net")]
+            [DataRow("abc.100@abc.com.au", "abc.100@abc.com.au")]
+            [DataRow("abc@1.com", "abc@1.com")]
+            [DataRow("abc@gmail.com.com", "abc@gmail.com.com")]
+            [DataRow("abc+100@gmail.com", "abc+100@gmail.com")]
+            [DataRow("abc", null)]
+            [DataRow("abc@.com.my", null)]
+            [DataRow("abc123@.com", null)]
+            [DataRow("abc123@.com.com", null)]
+            [DataRow("abc()*@gmail.com", null)]
+            [DataRow(".abc@abc.com", null)]
+            [DataRow("abc@%*.com", null)]
+            [DataRow("abc..2002@gmail.com", null)]
+            [DataRow("abc.@gmail.com", null)]
+            [DataRow("abc@abc@gmail.com", null)]
+            [DataRow("abc@gmail.com.1a", null)]
+            [DataRow("abc@gmail.com.aa.au", null)]
+
+            public void ValidateUserEmail(string a, string expected)
+            {
+                try
+                {
+                    var actual = RegexSample.ValidatingEmailId(a);
+                    Assert.AreEqual(expected, actual);
+                }
+                catch (CustomException actual)
+                {
+                    Assert.AreEqual(expected, actual.message);
+                }
+            }
+
+            //Validation for Phone Number
+            [TestMethod]
+            [DataRow("1 1000987267", "1 1000987267")]
+            [DataRow("91 9842905050", "91 9842905050")]
+            [DataRow("100 9842905050", "100 9842905050")]
+            [DataRow("919842905050", null)]
+            [DataRow("919842905", null)]
+            [DataRow("91 984290", null)]
+            [DataRow("91 984290505000000", null)]
+            public void ValidateUserPhoneNumber(string a, string expected)
+            {
+                try
+                {
+                    var actual = RegexSample.ValidatingPhoneNum(a);
+                    Assert.AreEqual(expected, actual);
+                }
+                catch (CustomException actual)
+                {
+                    Assert.AreEqual(expected, actual.message);
+                }
+            }
+
+            ////Validation for Password
+            [TestMethod]
+            [DataRow("Nike@123", "Nike@123")]
+            [DataRow("Nike#23", "Nike#123")]
+            [DataRow("Ni@ke1S ", "Ni@ke1S")]
+            [DataRow("@Nike453", "@Nike453")]
+            [DataRow("Ni-ke123", "Ni-ke123")]
+            [DataRow("Nike123)@1234", null)]
+            [DataRow("Ni@ke", null)]
+            [DataRow("Ni123", null)]
+            [DataRow("Nik@123", null)]
+            [DataRow("@ni#12kr", null)]
+            [DataRow(")nikeh12", null)]
+            [DataRow(")nikhil12", null)]
+            [DataRow("nik-ik@12S", null)]
+            [DataRow("niked@s", null)]
+
+            public void ValidateUserPassword(string a, string expected)
+            {
+                try
+                {
+                    var actual = RegexSample.ValidatingPassWord(a);
+                    Assert.AreEqual(expected, actual);
+                }
+                catch (CustomException actual)
+                {
+                    Assert.AreEqual(expected, actual.message);
+                }
+            }
+
+
+        }
+
+    }
+}
